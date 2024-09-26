759e08fa1bdb274b561a0b19e2fea1bc1705fd92
sanchezjuliomarcelo <sanchezjuliomarcelo@gmail.com>
2024-09-26 20:44:05 -0300

    2

--- api/login.js
+++ api/login.js
@@ -1,15 +1,30 @@
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const { email, password } = req.body;
+// api/login.js
+import { json } from 'body-parser';
+import axios from 'axios';
 
-        // Verificar si el email y la contraseña coinciden con las variables de entorno
-        if (email === process.env.USUARIO && password === process.env.CLAVE) {
-            return res.status(200).json({ message: 'Login exitoso' });
-        } else {
-            return res.status(401).json({ message: 'Credenciales incorrectas' });
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, password } = req.body;
+
+    const apiToken = process.env.KEY;
+    const apiSecret = process.env.SECRET;
+
+    // Lógica de autenticación con Zippin usando axios
+    try {
+      const response = await axios.get('https://api.zippin.com.ar/v2/shipments', {
+        headers: {
+          'Authorization': `Basic ${Buffer.from(`${apiToken}:${apiSecret}`).toString('base64')}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
         }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Método ${req.method} no permitido`);
+      });
+      res.status(200).json(response.data);
+    } catch (error) {
+      res.status(500).json({ message: 'Error en la autenticación' });
     }
+  } else {
+    // Método no permitido
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Método ${req.method} no permitido`);
+  }
 }
